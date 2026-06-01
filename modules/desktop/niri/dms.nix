68961c3e97d41755b9b2654d38c351d98ee7a7eb
Tias <timur.buchkin@mail.ru>
2026-06-01 13:36:19 +0300

    update xray config and niri dms settings

--- modules/desktop/niri/dms.nix
+++ modules/desktop/niri/dms.nix
@@ -3,13 +3,14 @@
     programs.dank-material-shell = {
       enable = true;
       enableSystemMonitoring = true;
-      enableDynamicTheming = false;
+      enableDynamicTheming = true;
       dgop.package = inputs.dgop.packages.${pkgs.stdenv.hostPlatform.system}.default;
       niri = {
 	enableSpawn = true;
       };
       plugins = {
 	dankBatteryAlerts.enable = true;
+	dankPomodoroTimer.enable = true;
 	dockerManager.enable = true;
 	emojiLauncher.enable = true;
       };
