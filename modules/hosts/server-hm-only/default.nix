c927f386882748af55f4916c127e76589a1e3c23
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-06-29 21:36:05 +0300

    change server-hm-only to machine name

--- modules/hosts/server-hm-only/default.nix
+++ modules/hosts/server-hm-only/default.nix
@@ -6,7 +6,7 @@
   ];
 in {
   flake = {
-    homeConfigurations.server-hm-only = config.flake.lib.mkSystems.linuxHMOnly "sdg-robot-bl-vla" {username = "tabuchkin";};
+    homeConfigurations.sdg-robot-bl-vla = config.flake.lib.mkSystems.linuxHMOnly "sdg-robot-bl-vla" {username = "tabuchkin";};
     modules.homeManager."hosts/sdg-robot-bl-vla" = {
       imports = config.flake.lib.collectHomeModules config modules;
     };
