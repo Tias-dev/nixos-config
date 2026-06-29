60688c0c45c624a1a83c1fe9fc309f1832bd790e
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-06-29 21:40:15 +0300

    change server hostname

--- modules/hosts/server-hm-only/default.nix
+++ modules/hosts/server-hm-only/default.nix
@@ -6,7 +6,7 @@
   ];
 in {
   flake = {
-    homeConfigurations.sdg-robot-bl-vla.vla.yp-c.yandex.net = config.flake.lib.mkSystems.linuxHMOnly "sdg-robot-bl-vla.vla.yp-c.yandex.net" {username = "tabuchkin";};
+    homeConfigurations."sdg-robot-bl-vla.vla.yp-c.yandex.net" = config.flake.lib.mkSystems.linuxHMOnly "sdg-robot-bl-vla.vla.yp-c.yandex.net" {username = "tabuchkin";};
     modules.homeManager."hosts/sdg-robot-bl-vla.vla.yp-c.yandex.net" = {
       imports = config.flake.lib.collectHomeModules config modules;
     };
