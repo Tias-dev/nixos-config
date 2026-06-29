7436eed95e8cc2c3d05df0c0bc31490957ef3916
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-06-29 17:35:48 +0300

    update nixvim

--- modules/dev/neovim.nix
+++ modules/dev/neovim.nix
@@ -5,4 +5,10 @@
     home.packages = [neovim];
     programs.zsh.sessionVariables.EDITOR = "nvim";
   };
+  flake.modules.homeManager.neovim-no-clangd-index = {pkgs, ...}: let
+      neovim = inputs.tias-nixvim-no-clangd-indexing.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  in {
+    home.packages = [neovim];
+    programs.zsh.sessionVariables.EDITOR = "nvim";
+  };
 }
