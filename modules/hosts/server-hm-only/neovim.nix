73f8f062aa151b9cf25d3ad2342ce7fa31940276
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-06-29 17:31:59 +0300

    fix hostname

--- modules/hosts/server-hm-only/neovim.nix
+++ modules/hosts/server-hm-only/neovim.nix
@@ -1,5 +1,5 @@
 {inputs, ...}: {
-  flake.modules.homeManager."hosts/server-hm-only" = {pkgs, ...}: let
+  flake.modules.homeManager."hosts/sdg-robot-bl-vla" = {pkgs, ...}: let
     neovim = inputs.tias-nixvim-no-clangd-indexing.packages.${pkgs.stdenv.hostPlatform.system}.default;
   in {
     home.packages = [neovim];
