c9399ce63b7d8c15fa59a9e43383672ac190dddf
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-07-03 21:04:10 +0300

    add firerox extensions

--- modules/browser/firefox.nix
+++ modules/browser/firefox.nix
@@ -25,6 +25,18 @@
         DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
         DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
         SearchBar = "unified"; # alternative: "separate"
+
+        ExtensionSettings = {
+          "*".installation_mode = "blocked";
+          "uBlock0@raymondhill.net" = {
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          };
+          "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium/latest.xpi";
+          };
+        };
       };
 
       profiles = {
