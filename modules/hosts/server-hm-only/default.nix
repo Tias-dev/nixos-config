98f3a122646f6f3a0ab0df8889d1798beedbe879
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-07-14 13:09:31 +0300

    add server only tmux copy command option

--- modules/hosts/server-hm-only/default.nix
+++ modules/hosts/server-hm-only/default.nix
@@ -11,6 +11,7 @@ in {
     homeConfigurations."sdg-robot-bl-vla.vla.yp-c.yandex.net" = config.flake.lib.mkSystems.linuxHMOnly "sdg-robot-bl-vla.vla.yp-c.yandex.net" {username = "tabuchkin";};
     modules.homeManager."hosts/sdg-robot-bl-vla.vla.yp-c.yandex.net" = {
       imports = config.flake.lib.collectHomeModules config modules;
+      tmux.server-copy.command.enable = true;
     };
   };
 }
