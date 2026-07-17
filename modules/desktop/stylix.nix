9e91bf14a59b628ac10aa923f0bae614edf1422b
Timur Buchkin <tabuchkin@yandex-team.ru>
2026-07-17 16:53:49 +0300

    update inputs

--- modules/desktop/stylix.nix
+++ modules/desktop/stylix.nix
@@ -12,8 +12,8 @@
 
     stylix = {
       enable = true;
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-terminal-dark.yaml";
-      polarity = "dark";
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+      polarity = "light";
 
       targets = {
         firefox.profileNames = ["tias-dev"];
