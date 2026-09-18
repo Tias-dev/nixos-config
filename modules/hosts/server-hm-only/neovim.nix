9eb1e0a58597e0c3d6ae62752903254cbc47466d
Timur Buchkin <timur.buchkin@mail.ru>
2026-09-18 12:15:30 +0300

    add nvim with indexing package

--- modules/hosts/server-hm-only/neovim.nix
+++ modules/hosts/server-hm-only/neovim.nix
@@ -1,18 +1,36 @@
 {inputs, ...}: {
   config.flake.modules.homeManager."hosts/sdg-robot-bl-vla.vla.yp-c.yandex.net" = {pkgs, ...}: let
     system = pkgs.stdenv.hostPlatform.system;
+    neovim-common-opts = {
+      clangd.disable-auto-import = true;
+      langChanger.enable = false;
+      format.on_save.enable = false;
+
+      yaml.enable = true;
+      cpp.indent-namespace = true;
+    };
     neovim =
       inputs.tias-nixvim.lib.neovimWithChangedOptions system
       {
         clangd.disable-indexing = true;
-        clangd.disable-auto-import = true;
-        langChanger.enable = false;
-        format.on_save.enable = false;
-
-        yaml.enable = true;
-        cpp.indent-namespace = true;
-      };
+      }
+      // neovim-common-opts;
+    indexing-neovim =
+      inputs.tias-nixvim.lib.neovimWithChangedOptions system
+      {
+        clangd.disable-indexing = false;
+      }
+      // neovim-common-opts;
   in {
     neovim-package = neovim;
+    home.packages = with pkgs; [
+      (writers.writeBashBin "ivim"
+        /*
+        bash
+        */
+        ''
+          ${indexing-neovim}/bin/nvim "$@"
+        '')
+    ];
   };
 }
