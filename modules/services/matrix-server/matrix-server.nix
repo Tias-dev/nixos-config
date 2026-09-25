5b7cfe5d8e3c7aa3b60e69dccb894f12adc19761
Timur Buchkin <timur.buchkin@mail.ru>
2026-09-25 19:30:07 +0300

    add livekit setup for matrix

--- modules/services/matrix-server/matrix-server.nix
+++ modules/services/matrix-server/matrix-server.nix
@@ -9,6 +9,8 @@
       subdomain,
       matrixPort ? 8008,
       networkInterface ? "enp1s0",
+      max_upload_file_size ? "1G",
+      upload_timeout ? "600s",
     }: {
       config,
       pkgs,
@@ -16,7 +18,16 @@
     }: let
       fqdn = "${subdomain}.${config.networking.domain}";
       baseUrl = "https://${fqdn}";
-      clientConfig."m.homeserver".base_url = baseUrl;
+      clientConfig = {
+        "m.homeserver".base_url = baseUrl;
+        "org.matrix.msc3575.proxy".url = baseUrl; # for livekit calls
+        "org.matrix.msc4143.rtc_foci" = [
+          {
+            type = "livekit";
+            livekit_service_url = "${baseUrl}/livekit/jwt";
+          }
+        ];
+      };
       serverConfig."m.server" = "${fqdn}:443";
       mkWellKnown = data: ''
         default_type application/json;
@@ -59,10 +70,28 @@
             "= /.well-known/matrix/server".extraConfig = mkWellKnown serverConfig;
             "= /.well-known/matrix/client".extraConfig = mkWellKnown clientConfig;
           };
+          extraConfig = ''
+            client_max_body_size ${max_upload_file_size};
+
+            proxy_request_buffering off;
+
+            proxy_connect_timeout ${upload_timeout};
+            proxy_send_timeout    ${upload_timeout};
+            proxy_read_timeout    ${upload_timeout};
+          '';
         };
         "${fqdn}" = {
           enableACME = true;
           forceSSL = true;
+          extraConfig = ''
+            client_max_body_size ${max_upload_file_size};
+
+            proxy_request_buffering off;
+
+            proxy_connect_timeout ${upload_timeout};
+            proxy_send_timeout    ${upload_timeout};
+            proxy_read_timeout    ${upload_timeout};
+          '';
           locations = {
             "/".extraConfig = ''
               return 404;
@@ -83,14 +112,29 @@
               default_theme = "dark";
             };
           };
+          extraConfig = ''
+            client_max_body_size ${max_upload_file_size};
+
+            proxy_request_buffering off;
+
+            proxy_connect_timeout ${upload_timeout};
+            proxy_send_timeout    ${upload_timeout};
+            proxy_read_timeout    ${upload_timeout};
+          '';
         };
       };
       # matrix based on synapse config
       services.matrix-synapse = {
         enable = true;
+        withJemalloc = true;
         settings = {
           server_name = config.networking.domain;
           public_baseurl = baseUrl;
+          max_upload_size = max_upload_file_size;
+          experimental_features = {
+            "org.matrix.msc3401" = true; # Включает нативную поддержку звонков Matrix RTC
+            "org.matrix.msc3861" = true; # Требуется для поддержки авторизации в Element X (OIDC)
+          };
           listeners = [
             {
               port = matrixPort;
@@ -102,6 +146,12 @@
                 {
                   names = [
                     "client"
+                    "media"
+                  ];
+                  compress = false;
+                }
+                {
+                  names = [
                     "federation"
                   ];
                   compress = true;
@@ -127,6 +177,59 @@
       };
 
       imports = [
+        # enable live kit for element x calls
+        ({config, ...}: let
+          keyFile = "/run/livekit.key";
+        in {
+          services.livekit = {
+            enable = true;
+            openFirewall = true;
+            inherit keyFile;
+          };
+
+          services.lk-jwt-service = {
+            enable = true;
+            livekitUrl = "wss://${fqdn}/livekit/sfu";
+            inherit keyFile;
+          };
+          # generate the key when needed
+          systemd.services.livekit-key = {
+            before = ["lk-jwt-service.service" "livekit.service"];
+            wantedBy = ["multi-user.target"];
+            path = with pkgs; [livekit coreutils gawk];
+
+            script = ''
+              echo "Key missing, generating key"
+              echo "lk-jwt-service: $(livekit-server generate-keys | tail -1 | awk '{print $3}')" > "${keyFile}"
+            '';
+            serviceConfig.Type = "oneshot";
+            unitConfig.ConditionPathExists = "!${keyFile}";
+          };
+          # restrict access to livekit room creation to a homeserver
+          systemd.services.lk-jwt-service.environment.LIVEKIT_FULL_ACCESS_HOMESERVERS = "${fqdn}";
+          services.nginx.virtualHosts.${fqdn}.locations = {
+            "^~ /livekit/jwt/" = {
+              priority = 400;
+
+              proxyPass = "http://[::1]:${toString config.services.lk-jwt-service.port}/";
+            };
+
+            "^~ /livekit/sfu/" = {
+              extraConfig = ''
+                proxy_send_timeout 120;
+                proxy_read_timeout 120;
+                proxy_buffering off;
+
+                proxy_set_header Accept-Encoding gzip;
+                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Connection "upgrade";
+              '';
+              priority = 400;
+              proxyPass = "http://[::1]:${toString config.services.livekit.settings.port}/";
+              proxyWebsockets = true;
+            };
+          };
+        })
         # enable coturn for videocalls
         ({
           config,
